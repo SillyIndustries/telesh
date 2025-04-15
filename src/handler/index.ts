7b9bd326fbaa5a1440e45f35c4a4b365c3f9f7d3
be195 <ilya.wicopee@gmail.com>
2025-04-15 23:31:47 +0300

    remove http stuff, upload files to Telegram directly

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,23 +1,61 @@
 import { createHash } from 'crypto';
 import { InlineQueryContext } from '@mtcute/dispatcher';
 import { BotInline } from '@mtcute/core';
-import config from '../config.js';
 
-interface Response {
-  timeout: NodeJS.Timeout,
+import sh from './shat.js';
+
+interface Waiter {
+  timeout: NodeJS.Timeout;
   resolve: (_: boolean) => void;
 }
 
-export async function onInlineQuery(ctx: InlineQueryContext) {
-  const identifier = 'QUERY' + createHash('sha1').end(ctx.query).digest('hex');
+const TIMEOUT_MS = 1500;
+const map: Map<number, Waiter> = new Map;
 
+async function wait(id: number) {
+  if (map.has(id)) {
+    const timeout = map.get(id);
+    clearTimeout(timeout?.timeout);
+    timeout?.resolve(false);
+  }
+
+  return new Promise<boolean>((res) =>
+    map.set(id, {
+      timeout: setTimeout(() => res(true), TIMEOUT_MS),
+      resolve: res
+    })
+  );
+}
+
+export async function onInlineQuery(ctx: InlineQueryContext) {
   try {
+    if (!(await wait(ctx.user.id)))
+      throw new Error();
+
+    const identifier = 'shat_' + createHash('sha1').end(ctx.query).digest('hex');
+
+    const chatsounds = sh.new(ctx.query);
+    const buffer = await chatsounds.buffer({
+      format: 'ogg',
+      codec: 'libopus',
+      audioChannels: 2,
+      sampleRate: 48000
+    });
+
+    if (!buffer)
+      throw new Error();
+
+    const file = await ctx.client.uploadMedia({
+      file: buffer,
+      type: 'voice'
+    });
+
     await ctx.answer([
       BotInline.voice(
         identifier,
-        config.host + '/' + Buffer.from(ctx.query).toString('base64url') + '?' + Date.now(),
+        file.fileId,
         {
-          title: 'shat',
+          title: ctx.query,
           message: {
             type: 'media',
             text: ctx.query
@@ -27,5 +65,6 @@ export async function onInlineQuery(ctx: InlineQueryContext) {
     ], { cacheTime: 300 });
   } catch (err) {
     console.log(err);
+    return await ctx.answer([]);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
 import './handler/shat.js';
 import { start } from './client.js';
-import { listen } from './handler/store.js';
-listen();
 start();
